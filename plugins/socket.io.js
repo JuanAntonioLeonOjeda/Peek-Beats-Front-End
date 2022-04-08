2f1ec6787a44a605612cbbb12840b13033f9a059
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-08 22:00:33 +0100

    changes in socket io plugin

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 import Vue from 'vue'
 
-const socket = io('http://localhost:5000')
+const socket = io('https://peek-beats.herokuapp.com')
 Vue.prototype.$socket = socket
 
 export default socket
