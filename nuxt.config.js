1bd9885c08c4f6bc29fa837267c4e74e51279def
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-08 21:41:13 +0100

    nuxt config - changed io cors

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
     sockets: [
       {
         name: 'test',
-        url: 'http://localhost:5000/'
+        url: 'https://peek-beats.herokuapp.com/'
       }
     ],
     server: {
@@ -67,7 +67,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://peek-beats.herokuapp.com/api'
   },
 
   auth: {
