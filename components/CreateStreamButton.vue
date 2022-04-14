d2219211f5d5e4c86d26730b3cc3053576b727ff
David Pallarés <serallapdivad@gmail.com>
2022-04-14 11:03:43 +0100

    CreateStreamButton
    - Send to store the right object (stream.stream)

--- components/CreateStreamButton.vue
+++ components/CreateStreamButton.vue
@@ -66,8 +66,8 @@ export default {
       await this.$store.dispatch('createStream', genreId[0]._id)
       const roomId = uuidv4()
       const stream = await this.$store.dispatch('assignStreamRoom', roomId)
-      console.log(stream);
-      this.$store.commit('getStreamInfo', stream)
+      console.log('stream: ' + stream);
+      this.$store.commit('getStreamInfo', stream.stream)
       this.$store.commit('changeRole')
       this.$router.push({
         path: `/streams/${roomId}`
