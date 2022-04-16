87752a37e368a536d6067cb9adf2d7a68bc5a108
root <root@LAPTOP-LMJQULD0.localdomain>
2022-04-16 17:08:31 +0100

    user identified when joining room - before merge with branch wrtcTry

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://peek-beats-back-wrtc.herokuapp.com/api'
     // baseURL: 'https://peek-beats-back-wrtc.herokuapp.com/api'
   },
 
@@ -116,7 +116,7 @@ export default {
     }
   },
   env: {
-    WS_URL: process.env.WS_URL || 'http://localhost:5050/'
+    WS_URL: process.env.WS_URL || 'https://peer-js-server-prueba.herokuapp.com'
     // WS_URL: process.env.WS_URL || 'https://peer-js-server-prueba.herokuapp.com'
   }
 }
